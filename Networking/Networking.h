307b63ad108274654324aa79f1ffbc7a7f1ad723
wisesascha <wisesascha@gmail.com>
2015-12-26 10:50:00 -0800

    Initial Commit

---
+++ Networking/Networking.h
@@ -0,0 +1,19 @@
+//
+//  Networking.h
+//  Networking
+//
+//  Created by Sascha Wise on 12/26/15.
+//  Copyright © 2015 SW. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for Networking.
+FOUNDATION_EXPORT double NetworkingVersionNumber;
+
+//! Project version string for Networking.
+FOUNDATION_EXPORT const unsigned char NetworkingVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Networking/PublicHeader.h>
+
+
